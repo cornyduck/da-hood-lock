c9dd1635f7bfd752b1c6d75743dba9d97d22f88b
cornyduck scripts <corruptedrobloxjune@gmail.com>
2025-02-10 01:31:28 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-!KbAvG8DsN6g9KbA;G;j-s89ARGNAv9BNg3KNAvP84Nvg6sA9G!pwN0vT83N8bGB6!s3_A9Gszug9KBA_GRO4sssAD98GbA
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cornyduck/da-hood-lock/refs/heads/main/main.lua"))()
